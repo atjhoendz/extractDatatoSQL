b35c260b2b6cc1bc3b37133705f38f3bf8ea8384
atjhoendz <achunarmando@gmail.com>
2020-06-07 21:58:10 +0700

    update schema

--- schema.sql
+++ schema.sql
@@ -30,4 +30,29 @@ CREATE TABLE perKasus (
   FOREIGN KEY (kode_provinsi) REFERENCES perProvinsi(kode_provinsi)
 );
 
+DROP TABLE IF EXISTS waktu
+
+CREATE TABLE waktu (
+  id_waktu INT AUTO_INCREMENT PRIMARY KEY,
+  tahun VARCHAR(10),
+  bulan VARCHAR(10),
+  hari VARCHAR(10)
+);
+
+DROP TABLE IF EXISTS factTable
+
+CREATE TABLE factTable (
+  id_factTable INT AUTO_INCREMENT PRIMARY KEY,
+  id_waktu INT,
+  id_kasus INT,
+  jumlah_kasus_sembuh_perhari INT,
+  jumlah_kasus_meniggal_perhari INT,
+  jumlah_kasus_dirawat_perhari INT,
+  jumlah_negatif INT,
+  pdp INT,
+  odp INT,
+  FOREIGN KEY (id_waktu) REFERENCES waktu(id_waktu),
+  FOREIGN KEY (id_kasus) REFERENCES perKasus(id_kasus)
+);
+
 -- INSERTING DATA
