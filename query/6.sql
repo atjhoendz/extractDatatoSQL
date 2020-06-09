cbecbfc8bf07d535d1ae4c62d8cf3d5e90dc8eb2
atjhoendz <achunarmando@gmail.com>
2020-06-09 14:47:30 +0700

    nomor 6

--- query/6.sql
+++ query/6.sql
@@ -1,1 +1,19 @@
 -- Perhitungan rata-rata kasus spesimen diperiksa harian
+SELECT TOP 1
+	((
+		select
+			jumlah_kasus_diperiksa_spesimen
+		from
+			factTable t
+		where 
+			t.id_factTable = 98
+	)/
+	(
+		select
+			COUNT(*)
+		from 
+			factTable
+	)
+	) AS 'Rata-rata Kasus Diperiksa Spesimen'
+from 
+	factTable f;
