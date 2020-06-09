18c8628a32084f37d61d38588e7203b9bf446c60
arifrgilang <rmkspls@gmail.com>
2020-06-09 14:44:11 +0700

    added query(s)

--- query/9.sql
+++ query/9.sql
@@ -1,1 +1,13 @@
 -- Perhitungan kasus positif Warga Negara Asing (WNA)
+
+SELECT
+    COUNT(perKasus.wn) AS Total_WNA,
+    perProvinsi.nama_provinsi as Provinsi
+FROM
+    perKasus JOIN perProvinsi ON perKasus.kode_provinsi = perProvinsi.kode_provinsi
+WHERE
+    perKasus.wn = 2
+GROUP BY 
+    perProvinsi.nama_provinsi
+ORDER BY
+    perProvinsi.nama_provinsi DESC;
